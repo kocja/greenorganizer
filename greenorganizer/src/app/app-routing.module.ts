b77706875f9ad386f6554152dfd9ea9d0d86968b
jko120475 <jko120475@iet-gibb.ch>
2023-11-04 11:47:13 +0100

    add greenorganizer components

---
+++ greenorganizer/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {StartComponent} from "./start/start.component";
+import {AboutComponent} from "./about/about.component";
+import {CalculateComponent} from "./calculate/calculate.component";
+
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'calculate', component: CalculateComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: '**', redirectTo: '/start' } // Handle 404 - Page not found
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- greenorganizer/src/app/app.module.ts
+++ greenorganizer/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { SourcesComponent } from './sources/sources.component';
+import { AboutComponent } from './about/about.component';
+import { ResultComponent } from './calculate/result/result.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    CalculateComponent,
+    SourcesComponent,
+    AboutComponent,
+    ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
