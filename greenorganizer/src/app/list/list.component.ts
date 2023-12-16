613e56033ea16782c922af69521951818bb49e76
jko120475 <jko120475@iet-gibb.ch>
2023-12-16 14:17:55 +0100

    add fast fashion

--- greenorganizer/src/app/list/list.component.ts
+++ greenorganizer/src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {ClothingService} from "../clothing.service";
 import {Subscription} from "rxjs";
 
@@ -8,7 +8,6 @@ import {Subscription} from "rxjs";
   styleUrls: ['./list.component.css']
 })
 export class ListComponent  implements OnInit, OnDestroy {
-
   closet: any[] = [];
   private subscription: Subscription = new Subscription();
 
@@ -24,4 +23,22 @@ export class ListComponent  implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  calculate() {
+// Assuming this.closet is defined in your component
+
+// Array of fast fashion brands
+    const fastFashionBrands = ["Bershka", "H&M", "Zara"];
+
+// Filter items in this.closet that are fast fashion
+    const fastFashionItems = this.closet.filter(item => fastFashionBrands.includes(item.brand));
+
+// Log the result
+    if (fastFashionItems.length === 0) {
+      console.log("Does not contain fast fashion.");
+    } else if (fastFashionItems.length === 1) {
+      console.log("Contains 1 fast fashion item.");
+    } else {
+      console.log(`Contains ${fastFashionItems.length} fast fashion items.`);
+    }
+  }
 }
--- greenorganizer/src/app/start/check/check.component.ts
+++ greenorganizer/src/app/start/check/check.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./check.component.css']
 })
 export class CheckComponent {
-
 }
