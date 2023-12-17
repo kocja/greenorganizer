3d1a8039a8a3b6ec6f83e38c2c43e2073b6405be
jko120475 <jko120475@iet-gibb.ch>
2023-12-18 00:59:34 +0100

    almost final

--- greenorganizer/src/app/about/about.component.ts
+++ greenorganizer/src/app/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent {
 
 }
